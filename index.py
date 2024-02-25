dc99ea398a97a31f185df58c19d99b4a45a11afe
Otto <ottosaewong@gmail.com>
2024-02-25 10:37:02 +0000

    working test program with example .env

--- index.py
+++ index.py
@@ -1,1 +1,20 @@
-import rustplus
+import os
+import asyncio
+from rustplus import RustSocket
+from dotenv import load_dotenv
+
+load_dotenv()
+ip=os.environ.get("IP")
+port=os.environ.get("PORT")
+steamId=int(os.environ.get("STEAMID"))
+playerToken=int(os.environ.get("PLAYERTOKEN"))
+
+async def main():
+    socket = RustSocket(ip,port,steamId,playerToken)
+    await socket.connect()
+
+    print(f"It is {(await socket.get_time()).time}")
+
+    await socket.disconnect()
+
+asyncio.run(main())
