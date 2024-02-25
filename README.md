76c3e7c791f0fd76bad8606ec060d71b19330f67
Otto <ottosaewong@gmail.com>
2024-02-25 10:52:46 +0000

    update readme; make basic flask app that shows in game time.

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # SL RUST
 A Rust+ Python application
+
+## Get Stuff Set Up
+We use the JS stuff to get the player token since the Python one is more hassle, and doesn't actually work (for me).
+Run this:
+```
+npx @liamcottle/rustplus.js fcm-register
+```
+login through steam in the window that pops up.
+
+Then run:
+```
+npx @liamcottle/rustplus.js fcm-listen
+```
+And pair with some smart devive in game, in order to get your playerToken and playerId (Steam ID).
+Put these in the .env
--- index.py
+++ index.py
@@ -2,6 +2,7 @@ import os
 import asyncio
 from rustplus import RustSocket
 from dotenv import load_dotenv
+from flask import Flask
 
 load_dotenv()
 ip=os.environ.get("IP")
@@ -9,12 +10,19 @@ port=os.environ.get("PORT")
 steamId=int(os.environ.get("STEAMID"))
 playerToken=int(os.environ.get("PLAYERTOKEN"))
 
+app = Flask(__name__)
+
 async def main():
     socket = RustSocket(ip,port,steamId,playerToken)
     await socket.connect()
 
-    print(f"It is {(await socket.get_time()).time}")
-
+    time = (f"It is {(await socket.get_time()).time}")
     await socket.disconnect()
+    return time
 
-asyncio.run(main())
+@app.route("/")
+def hello_world():
+    return asyncio.run(main())
+
+if __name__ == '__main__':
+    app.run(debug=True)
