1209a62a78de95a58ccceb6d5fe8e0f58736ea52
Otto <ottosaewong@gmail.com>
2024-02-25 16:50:38 +0000

    gitignore rustplus config; add index html file with ability to add stuff to database.

--- index.py
+++ index.py
@@ -2,31 +2,42 @@ import os
 import asyncio
 from rustplus import RustSocket
 from dotenv import load_dotenv
-from flask import Flask
+from flask import Flask, render_template, request
 import sqlite3
 
 connection = sqlite3.connect('database.db')
-
+cur = connection.cursor()
+cur.execute("CREATE TABLE IF NOT EXISTS tbl_devices (id INTEGER PRIMARY KEY, name TEXT);")
 
 load_dotenv()
-ip=os.environ.get("IP")
-port=os.environ.get("PORT")
-steamId=int(os.environ.get("STEAMID"))
-playerToken=int(os.environ.get("PLAYERTOKEN"))
+ip = os.environ.get("IP")
+port = os.environ.get("PORT")
+steamId = int(os.environ.get("STEAMID"))
+playerToken = int(os.environ.get("PLAYERTOKEN"))
 
 app = Flask(__name__)
 
-async def main():
-    socket = RustSocket(ip,port,steamId,playerToken)
-    await socket.connect()
-
-    time = (f"It is {(await socket.get_time()).time}")
-    await socket.disconnect()
+async def get_time():
+    rust_socket = RustSocket(ip, port, steamId, playerToken)
+    await rust_socket.connect()
+    time = (await rust_socket.get_time()).time
+    await rust_socket.disconnect()
     return time
 
 @app.route("/")
-def hello_world():
-    return asyncio.run(main())
+def index():
+    time = asyncio.run(get_time())
+    return render_template("index.html", time=time)
+
+@app.route("/add_device", methods=["POST"])
+def add_device():
+    device_id = request.form["device_id"]
+    device_name = request.form["device_name"]
+
+    cur.execute("INSERT INTO tbl_devices (id, name) VALUES (?, ?)", (device_id, device_name))
+    connection.commit()
+
+    return "Device added successfully."
 
 if __name__ == '__main__':
     app.run(debug=True)
