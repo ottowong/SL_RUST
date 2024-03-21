68cb1e252789d61d3b800b0670b1f87deb645691
Otto <ottosaewong@gmail.com>
2024-03-21 09:42:07 +0000

    display shop icon for all markers

--- static/js/canvas.js
+++ static/js/canvas.js
@@ -1,3 +1,5 @@
+var all_markers;
+
 window.onload = function () {
     var map = L.map('map-canvas',{ 
         crs: L.CRS.Simple, // use px coords
@@ -8,9 +10,9 @@ window.onload = function () {
 
     var shopIcon = L.icon({
         iconUrl: '../static/shop.png',
-        iconSize: [25, 25], // original size of the icon
-        iconAnchor: [0, 0], // point of the icon which will correspond to marker's location
-        popupAnchor: [0, 0], // point from which the popup should open relative to the iconAnchor
+        iconSize: [25, 25],
+        iconAnchor: [0, 0],
+        popupAnchor: [0, 0],
     });
 
     var imageUrl = '../static/map.png';
@@ -18,23 +20,40 @@ window.onload = function () {
     let height = 2000
     let boundPadding = 1000
 
-    let halfwidth = width / 2
-    let halfheight = height / 2
-
     var imageSize = [height, width];
     var imageBounds = [[0, 0], imageSize];
 
     L.imageOverlay(imageUrl, imageBounds).addTo(map);
 
-    //// rotated marker
-    // L.rotatedMarker([0,0], {
+    // rotated marker example
+    // let marko = L.rotatedMarker([0,0], {
     //     icon: shopIcon,
     //     rotationAngle: 90,
     // }).addTo(map);
+    // marko.setRotationAngle(180)
 
     var maxBounds = L.latLngBounds([
         [0 - boundPadding, 0 - boundPadding], 
         [height + boundPadding, width + boundPadding]
     ]);
     map.setMaxBounds(maxBounds);
+
+    function updateMarkers() {
+        console.log("updateMarkers")
+        // Remove existing markers
+        map.eachLayer(function (layer) {
+            if (layer instanceof L.Marker) {
+                map.removeLayer(layer);
+            }
+        });
+
+        // Add markers from the coordinates array
+        all_markers.forEach(function(coord) {
+            var y = coord[1] / 4500 * 2000
+            var x = coord[2] / 4500 * 2000
+            L.marker([x,y], {icon: shopIcon}).addTo(map);
+        });
+    }
+
+    setInterval(updateMarkers, 1000);
 };
