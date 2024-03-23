d104034be23f2e1e1d496e43d91ff9b73224fbab
Otto <ottosaewong@gmail.com>
2024-03-23 10:06:30 +0000

    tidying

--- static/js/canvas.js
+++ static/js/canvas.js
@@ -44,6 +44,36 @@ var chinookIcon = L.icon({
     iconAnchor:   [0, 0],
 });
 
+const colourMap = {
+    0: { main: "black", second: "white" },
+    1: {
+        0: { main: mainYellow, second: secondYellow },
+        1: { main: mainBlue, second: secondBlue },
+        2: { main: mainGreen, second: secondGreen },
+        3: { main: mainRed, second: secondRed },
+        4: { main: mainMagenta, second: secondMagenta },
+        5: { main: mainCyan, second: secondCyan }
+    }
+};
+
+const textMap = {
+    0: "",
+    1: {
+        0: "",
+        1: "&#xf155;",
+        2: "&#xf015;",
+        3: "&#xf4cd;",
+        4: "&#xf05b;",
+        5: "&#xf132;",
+        6: "&#xf54c;",
+        7: "&#xf236;",
+        8: "Z",
+        9: "&#xf19b;",
+        10: "&#xf6fc;",
+        11: "&#xf03e;"
+    }
+};
+
 var map;
 
 var player_to_track = "";
@@ -63,35 +93,6 @@ function updateNotes(newNotes) {
         let icon = createCustomIcon(shopGreen,shopGreen,"?", "red")
         let colour = "white";
         let colour2 = "black";
-        const colourMap = {
-            0: { main: "black", second: "white" },
-            1: {
-                0: { main: mainYellow, second: secondYellow },
-                1: { main: mainBlue, second: secondBlue },
-                2: { main: mainGreen, second: secondGreen },
-                3: { main: mainRed, second: secondRed },
-                4: { main: mainMagenta, second: secondMagenta },
-                5: { main: mainCyan, second: secondCyan }
-            }
-        };
-
-        const textMap = {
-            0: "",
-            1: {
-                0: "",
-                1: "&#xf155;",
-                2: "&#xf015;",
-                3: "&#xf4cd;",
-                4: "&#xf05b;",
-                5: "&#xf132;",
-                6: "&#xf54c;",
-                7: "&#xf236;",
-                8: "Z",
-                9: "&#xf19b;",
-                10: "&#xf6fc;",
-                11: "&#xf03e;"
-            }
-        };
 
         switch (note[0]) {
             case 0: // death marker
@@ -99,20 +100,18 @@ function updateNotes(newNotes) {
             case 1: // normal marker
                 const colors = colourMap[1][note[4]] || colourMap[0]; // Default to black and white if note[4] is invalid
                 const { main, second } = colors;
-                const text = textMap[1][note[3]] || textMap[0]; // Default text
+                const text = textMap[1][note[3]] || textMap[0];
                 icon = createCustomIcon(main, second, text);
                 let current_pin = L.rotatedMarker([x, y], {
                     icon: icon
                 });
                 current_pin.addTo(map);
                 note_pins.push(current_pin);
+                // do something to make sure they are in front of shop markers
                 break;
             default:
                 break;
         }
-
-
-        
     }
 }
 
@@ -286,5 +285,4 @@ window.onload = function () {
         [pixelHeight + boundPadding, pixelWidth + boundPadding]
     ]);
     map.setMaxBounds(maxBounds);
-    // setInterval(updateMarkers, 2000);
 };
